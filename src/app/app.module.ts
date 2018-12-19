b6c472368e3ff78cba33c4374ccd79082c558b6d
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2018-12-20 01:51:50 +0200

    initial project tree structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ServicesModule } from './modules/services/services.module';
+import { TemplatesModule } from './templates/templates/templates.module';
+
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServicesModule,
+    TemplatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
