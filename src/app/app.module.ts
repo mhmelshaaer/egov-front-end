bc4a0187bfa73640778bbcff0b2886543d70c8d0
MouhammedElshaaer <mouhammedelshaaer@stud.fci-cu.edu.eg>
2018-12-20 02:02:05 +0200

    initial project tree structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ServicesModule } from './modules/services/services.module';
-import { TemplatesModule } from './templates/templates/templates.module';
+import { TemplatesModule } from './templates/templates.module';
 
 
 import { AppComponent } from './app.component';
